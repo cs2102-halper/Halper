d8caecb2b0e24f663d988289fda066165bb22dce
rajdeepsh <rajdeepsh95@gmail.com>
2019-04-04 22:18:47 +0800

    Auto stash before merge of "master" and "origin/master"

--- App/routes/select.js
+++ App/routes/select.js
@@ -2,14 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const { Pool } = require('pg')
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'halper',
-//   password: 'postgres',
-//   port: 5432,
-// })
-
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 });
